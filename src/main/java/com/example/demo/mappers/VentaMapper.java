b8f35fbde30236be27cfeb7155d0c0113f42b4d4
nicoRepos <119765859+noarphelp@users.noreply.github.com>
2025-08-04 22:51:55 +0200

    Update VentaMapper.java

--- src/main/java/com/example/demo/mappers/VentaMapper.java
+++ src/main/java/com/example/demo/mappers/VentaMapper.java
@@ -1,28 +1,7 @@
 package com.example.demo.mappers;
 
-
-import com.example.demo.dtos.VentaDTO;
-import com.example.demo.entities.Venta;
-
 public class VentaMapper {
 
-    public static Venta toEntity(VentaDTO dto) {
-        if (dto == null) return null;
-        Venta venta = new Venta();
-        venta.setId(dto.getId());
-        venta.setFecha(dto.getFecha());
-        venta.setSucursal(SucursalMapper.toEntity(dto.getSucursal()));
-        // No seteamos detalles aquí
-        return venta;
-    }
 
-    public static VentaDTO toDTO(Venta entity) {
-        if (entity == null) return null;
-        VentaDTO dto = new VentaDTO();
-        dto.setId(entity.getId());
-        dto.setFecha(entity.getFecha());
-        dto.setSucursal(SucursalMapper.toDTO(entity.getSucursal()));
-        return dto;
-    }
 
 }
