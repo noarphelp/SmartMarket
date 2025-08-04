82d2c1b1da4415126d897b3ede486b2dc39c3da0
nicoRepos <119765859+noarphelp@users.noreply.github.com>
2025-08-04 22:51:05 +0200

    Update SucursalMapper.java

--- src/main/java/com/example/demo/mappers/SucursalMapper.java
+++ src/main/java/com/example/demo/mappers/SucursalMapper.java
@@ -1,30 +1,10 @@
 package com.example.demo.mappers;
 
 
-import com.example.demo.dtos.SucursalDTO;
-import com.example.demo.entities.Sucursal;
 
 public class SucursalMapper {
 
-    public static Sucursal toEntity(SucursalDTO dto) {
-        if (dto == null) return null;
-        Sucursal entity = new Sucursal();
-        entity.setId(dto.getId());
-        entity.setNombre(dto.getNombre());
-        entity.setDireccion(dto.getDireccion());
-        entity.setCiudad(dto.getCiudad());
-        return entity;
-    }
-
-    public static SucursalDTO toDTO(Sucursal entity) {
-        if (entity == null) return null;
-        SucursalDTO dto = new SucursalDTO();
-        dto.setId(entity.getId());
-        dto.setNombre(entity.getNombre());
-        dto.setDireccion(entity.getDireccion());
-        dto.setCiudad(entity.getCiudad());
-        return dto;
-    }
+   
 
 }
 
